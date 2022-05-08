0234b4e2b1e346f5ab58aa14ec995d643aacf1eb
Gautham <appskans2017@gmail.com>
2022-05-08 21:38:48 +0300

    created components for navbar and home banner img

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBYYo_KS-8Wnay9Yu4xsyUhGHyICWW1vkc",
+    authDomain: "clone-react-63f94.firebaseapp.com",
+    projectId: "clone-react-63f94",
+    storageBucket: "clone-react-63f94.appspot.com",
+    messagingSenderId: "1095242902903",
+    appId: "1:1095242902903:web:4a6f4bae18dd2cd98b8572",
+    measurementId: "G-8BG8VP3WYB"
+  };
