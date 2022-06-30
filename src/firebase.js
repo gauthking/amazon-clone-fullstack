7bde0dd93567219ba83be0214daf50702a473ebe
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2022-06-30 22:06:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBYYo_KS-8Wnay9Yu4xsyUhGHyICWW1vkc",
-  authDomain: "clone-react-63f94.firebaseapp.com",
-  projectId: "clone-react-63f94",
-  storageBucket: "clone-react-63f94.appspot.com",
-  messagingSenderId: "1095242902903",
-  appId: "1:1095242902903:web:4a6f4bae18dd2cd98b8572",
-  measurementId: "G-8BG8VP3WYB"
+  //you can get this from your project settings on firebase
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
